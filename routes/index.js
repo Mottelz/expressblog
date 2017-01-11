78b67a2992a25c39e31b63db18b5f64dfffd3f07
Mottelz <shachna@gmail.com>
2017-01-11 16:52:57 -0500

    Commented out Mongoose for now.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
 
 
 //Using mongoose to accept mongoDB
-var dburl = 'mongodb://mottel:H3r3inbepoints@ds145168.mlab.com:45168/helpless';
-mongoose.connect(dburl);
+//var dburl = 'mongodb://mottel:H3r3inbepoints@ds145168.mlab.com:45168/helpless';
+//mongoose.connect(dburl);
 
 //Schema Post
-var BlogpostSchema = new Schema({
+/*var BlogpostSchema = new Schema({
     title : String,
     body : String,
     pubdate : { type: Date, default: Date.now },
@@ -17,7 +17,7 @@ var BlogpostSchema = new Schema({
     psid: Number
 });
 
-var BlogpostModel = mongoose.model('Blogpost', BlogpostSchema);
+var BlogpostModel = mongoose.model('Blogpost', BlogpostSchema);*/
 
 
 /* GET post page. (WIP) */
@@ -26,7 +26,6 @@ router.get('/:psid?', function(req, res, next) {
 
         var file_path = "../content/essays/" + postid + ".json";
         var content = require(file_path);
-
 
         res.render('index', { title: content.title, date: content.date, post: content.post, webtitle: content.webtitle});
     });
