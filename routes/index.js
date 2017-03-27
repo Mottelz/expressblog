7e259c11cd627358ef5cae47384e239785e32a40
Mottelz <shachna@gmail.com>
2017-03-27 14:20:48 -0400

    Added new post and changed the rollover text for the header and updated the modules.

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ var BlogpostModel = mongoose.model('Blogpost', BlogpostSchema);*/
 
 /* GET post page.*/
 router.get('/:psid?', function(req, res, next) {
-       var postid = req.params.psid || "7"; // if no psid is given in url, default to post 7
+       var postid = req.params.psid || "8"; // if no psid is given in url, default to post 8
 
         var file_path = "../content/essays/" + postid + ".json";
         var content = require(file_path);
