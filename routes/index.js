98685fcc856ff8fbd2570c4609294ff887d3cf9e
Mottelz <mottelz@users.noreply.github.com>
2017-12-21 15:49:17 -0500

    Started adding new developer's journal section

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ var BlogpostModel = mongoose.model('Blogpost', BlogpostSchema);*/
 
 /*Homepage Render*/
 router.get('/', function(req, res, next) {
-    res.render('home', {webtitle: "Mottelz - Home",});
+    //res.render('home', {webtitle: "Mottelz - Home"});
 });
 
 
