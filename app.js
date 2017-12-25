32868d7a1534c115e86923c3b1af2ab3ccd2eecd
Mottelz <mottelz@users.noreply.github.com>
2017-12-25 03:10:05 -0500

    Cleaned up so that it's just a dev blog.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 
 //routes
 var index = require('./routes/index');
-// var users = require('./routes/users');
 var app = express();
 
 // view engine setup
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,20 @@
 var express = require('express');
 var router = express.Router();
-//var mongoose = require('mongoose');
-//var Schema = mongoose.Schema;
-
-
-//Using mongoose to accept mongoDB
-//var dburl = 'mongodb://mottel:H3r3inbepoints@ds145168.mlab.com:45168/helpless';
-//mongoose.connect(dburl);
-
-//Schema Post
-/*var BlogpostSchema = new Schema({
-    title : String,
-    body : String,
-    pubdate : { type: Date, default: Date.now },
-    author : String,
-    psid: Number
-});
-
-var BlogpostModel = mongoose.model('Blogpost', BlogpostSchema);*/
-
-
 
 /*Homepage Render*/
 router.get('/', function(req, res, next) {
     // res.render('home', {webtitle: "Mottelz - Home"});
     var file_path = "../content/dev/0.json";
     var content = require(file_path);
-    res.render('index', { type: 'dev', title: content.title, date: content.date, post: content.post, webtitle: content.webtitle, author: content.author, prev: content.prev, next: content.next});
+    res.render('index', {
+        type: 'dev',
+        title: content.title,
+        date: content.date,
+        post: content.post,
+        webtitle: content.webtitle,
+        author: content.author,
+        prev: content.prev,
+        next: content.next});
 });
 
 
@@ -36,19 +24,20 @@ router.get('/', function(req, res, next) {
 router.get('/:posttype/:psid?', function(req, res, next) {
         var postid = req.params.psid;
         var posttype = req.params.posttype;
-
-
         var file_path = "../content/" + posttype + "/" + postid + ".json";
         var content = require(file_path);
 
 
         if (posttype == "essays" || posttype == "dev") {
-        res.render('index', { type: posttype, title: content.title, date: content.date, post: content.post, webtitle: content.webtitle, author: content.author, prev: content.prev, next: content.next}); }
-
-        if (posttype == "poems"){
-            res.render('poems',{title: content.title, poem: content.poem,})
-        }
-
+        res.render('index', {
+            type: posttype,
+            title: content.title,
+            date: content.date,
+            post: content.post,
+            webtitle: content.webtitle,
+            author: content.author,
+            prev: content.prev,
+            next: content.next}); }
 
     });
 
